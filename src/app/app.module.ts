037f541629dffc88a6ba4ef9e9857ad658630f59
Daniel <danieldanieltalortalor@gmail.com>
2018-04-01 02:10:54 +0300

    Made Pitboss In Out Page - Daniel
    Signed-off-by: Daniel <danieldanieltalortalor@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { PitBosInOutComponent } from './components/pit-bos-in-out/pit-bos-in-out.component';
+import { PitBossBubbleComponent } from './components/pit-bos-in-out/components/pit-boss-bubble/pit-boss-bubble.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PitBosInOutComponent,
+    PitBossBubbleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/pit-bos-in-out/pit-bos-in-out.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pit-bos-in-out',
+  templateUrl: './pit-bos-in-out.component.html',
+  styleUrls: ['./pit-bos-in-out.component.css']
+})
+export class PitBosInOutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
